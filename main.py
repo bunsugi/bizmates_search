1bc0f913665c0507495626e0c99a7ff35772e9a6
bunsugi <65064434+ay-sugiura@users.noreply.github.com>
2023-07-17 18:27:00 +0900

    空き枠を1週間分取得

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 from selenium import webdriver
 from time import sleep
 from dotenv import load_dotenv
+# ------- 追加部分 -------
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# ------- 追加部分 -------
 
 load_dotenv()
 
@@ -11,10 +17,7 @@ def main():
     login_url = os.getenv('LOGIN_URL')
     email = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
-
-    print(os.getenv('LOGIN_URL'))
-    print(os.getenv('EMAIL'))
-    print(os.getenv('PASSWORD'))
+    teacher_url = os.getenv('TEACHER_URL')
 
     driver.get(login_url)
     driver.save_screenshot('test.png')
@@ -25,14 +28,42 @@ def main():
 
     # ログイン
     driver.find_element_by_id('login_submit').click()
-
-    driver.save_screenshot('test2_after_submit.png')
-
-    teacher_url = 'https://www.bizmates.jp/MyBizmates/student/schedule'
     driver.get(teacher_url)
-    driver.save_screenshot('test3.png')
+    driver.save_screenshot('test3_teacher.png')
+
+    trainer_details = driver.find_elements_by_css_selector('a.trainer_detail')
+
+    trainer_details[6].click()
+
+    # 最大の読み込み時間を設定 今回は最大30秒待機できるようにする
+    wait = WebDriverWait(driver=driver, timeout=30)
+    # トレーナー詳細の上のタブが出るまで待機
+    wait.until(EC.visibility_of_element_located((By.ID, 'tabs')))
+    driver.save_screenshot('trainer_details.png')
+    # 「レッスン予約」をクリック
+    driver.find_element_by_css_selector("#tabs > ul > li:nth-child(2)").click()
+    # スケジュール表が表示されるまで待機
+    wait.until(EC.visibility_of_element_located((By.ID, 'trainerScheduleSearchResult')))
+    driver.save_screenshot('trainer_details2.png')
+
+    # 朝の時間帯ボタンをクリック
+    time_zones = driver.find_elements_by_css_selector('#trainer_time_period_list td')
+    number_of_available = []
+
+    for time_zone in time_zones:
+        time_zone.click()
+        # スケジュール表が表示されるまで待機
+        sleep(0.5)
+        wait.until(EC.presence_of_element_located((By.ID, 'trainerScheduleSearchResult')))
+        # id=trainerScheduleSearchResultの中の〇（i要素）の要素をすべて取得し配列に入れる
+        a = driver.find_elements_by_css_selector('#trainerScheduleSearchResult i')
+
+        number_of_available.append(len(a))
+        print(len(a))
+
+    print(number_of_available)
+    driver.quit()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
